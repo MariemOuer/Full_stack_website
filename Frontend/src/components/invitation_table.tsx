20c5bd7d531c1f26fce33852019a0767ccc9d904
miri-j-kim <miri.kim@ucalgary.ca>
2025-03-16 21:07:48 -0600

    basic invitation table css for pc and mobile

--- Frontend/src/components/invitation_table.tsx
+++ Frontend/src/components/invitation_table.tsx
@@ -1,4 +1,6 @@
+import React, { useState } from 'react';
 import { Invitation } from '@src/models/invitation';
+import '../styles/invitation_table.css';
 
 type InvitationTableProps = {
   itineraryUUID: string;
@@ -7,32 +9,45 @@ type InvitationTableProps = {
 };
 
 const InvitationTable: React.FC<InvitationTableProps> = ({ itineraryUUID, itineraryPartySize, invitations }) => {
-  return (
-    <div>
-      <h2>Guest List</h2>
+  const [selectedGuest, setSelectedGuest] = useState<string | null>(null);
+  const [openAddGuest, setOpenAddGuest] = useState(false);
 
-      <div>
-        <a href="#" className="hover:underline">
-          Add/Remove Guest
+  const togglePopup = () => {
+    setOpenAddGuest(!openAddGuest);
+  }
+
+  const handleRowClick = (guestId: string) => {
+    setSelectedGuest(selectedGuest === guestId ? null : guestId);
+  };
+
+  return (
+    <div className='invite-table-component'>
+      <h2 className='page-header'>Guest List</h2>
+
+      <div className='invite-table-options'>
+        <a href='#' className="hover:underline" onClick={togglePopup}>
+          Add Guest
         </a>
-        <a href="#" className="hover:underline">
+        <a href='#' className="hover:underline">
           Send Reminder To Pending Guests
         </a>
       </div>
 
-      <table>
+      <table className='invite-table'>
         <thead>
-          <tr>
+          <tr className='invite-table-header'>
             <th>RSVP Date</th>
             <th>Name</th>
             <th>Email</th>
             <th>Party Of</th>
             <th>Status</th>
+            <th/>
           </tr>
         </thead>
         <tbody>
           {invitations.map((guest) => (
-            <tr key={guest.id}>
+            <React.Fragment key={guest.id}>
+            <tr onClick={() => handleRowClick(guest.id.toString())}>
               <td>{new Date(guest.rsvpDeadline).toLocaleDateString()}</td>
               <td>{guest.name}</td>
               <td>
@@ -40,7 +55,36 @@ const InvitationTable: React.FC<InvitationTableProps> = ({ itineraryUUID, itiner
               </td>
               <td>2</td>
               <td>{guest.status}</td>
+              <td><img src='./trash-red.png' alt="trashcan" className='trash-red'/></td>
             </tr>
+            {selectedGuest === guest.id.toString() && (
+                <tr className='dropdown-row'>
+                  <td colSpan={6}>
+                    <div className="dropdown-content">
+                      <div className='dropdown-content-row'>
+                        <div>
+                          <span className='dropdown-content-header'>Email</span> 
+                          <a href={`mailto:${guest.email}`}>{guest.email}</a>
+                        </div>
+                        <div>
+                          <span className='dropdown-content-header'>Party Of</span>
+                          2
+                        </div>
+                      </div>
+                      <div className='dropdown-content-row'>
+                        <div>
+                          <span className='dropdown-content-header'>Status</span>
+                          {guest.status}
+                        </div>
+                        <div>
+                          <img src='./trash-red.png' alt="trashcan" className='trash-red'/>
+                        </div>
+                      </div>
+                    </div>
+                  </td>
+                </tr>
+              )}
+            </React.Fragment>
           ))}
         </tbody>
       </table>
